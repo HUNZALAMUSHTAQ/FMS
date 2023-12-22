aefcc40430b64b4859a7ee463cf9fa95ae59bb70
Hunzala Mushtaq <hanzla1702@gmail.com>
2023-12-22 18:58:30 +0500

    Added Modal doc add

--- src/view/pages/documents/index.jsx
+++ src/view/pages/documents/index.jsx
@@ -1,7 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { FormattedMessage } from "react-intl";
 
-import { Space, Table, Tag, Button } from "antd";
+import {
+  Row,
+  Col,
+  Divider,
+  Form,
+  Space,
+  Table,
+  Select,
+  Tag,
+  Input,
+  DatePicker,
+  TimePicker,
+  Button,
+  Modal,
+  message,
+  Upload,
+} from "antd";
 
+import { RiCloseFill, RiCalendarLine } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
+import { UploadOutlined } from "@ant-design/icons";
+
+const uploadProps = {
+  name: "file",
+  action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 const columns = [
   {
     title: "Document Title",
@@ -134,11 +172,123 @@ const data = [
   // ... (Include additional rows as needed)
 ];
 export default function Documents() {
+  const [contactModalVisible, setContactModalVisible] = useState(false);
+  const [docTitle, setDocTitle] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+
+  const updateUserInfo = () => {
+    const obj = { id: user?.id };
+    if (docTitle) {
+      obj.docTitle = docTitle;
+    }
+    if (lastName) {
+      obj.lastName = lastName;
+    }
+    if (phoneNumber) {
+      obj.phoneNumber = phoneNumber;
+    }
+    if (address) {
+      obj.address = address;
+    }
+    dispatch(updateUserInformation(obj));
+    setAddress("");
+    setFirstName("");
+    setLastName("");
+    setPhoneNumber("");
+    contactModalCancel();
+  };
+  const contactModalShow = () => {
+    setContactModalVisible(true);
+  };
+
+  const contactModalCancel = () => {
+    setContactModalVisible(false);
+  };
+
   return (
     <>
-      {" "}
+      <Modal
+        title="Upload Document"
+        width={416}
+        centered
+        visible={contactModalVisible}
+        onCancel={contactModalCancel}
+        footer={null}
+        closeIcon={
+          <RiCloseFill className="remix-icon text-color-black-100" size={24} />
+        }
+      >
+        <Form
+          layout="vertical"
+          name="basic"
+          initialValues={{
+            remember: true,
+            docTitle,
+            lastName,
+            address,
+            phoneNumber,
+          }}
+        >
+          <Form.Item label="Document Title" name="docTitle">
+            <Input
+              value={docTitle}
+              onChange={(e) => setDocTitle(e.target.value)}
+            />
+          </Form.Item>
+
+          <Form.Item label="Department Name" name="deptName">
+            <Select
+              defaultValue="IT"
+              options={[
+                { value: "IT", label: "IT" },
+                { value: "Admin", label: "Admin" },
+                { value: "Supply Chain", label: "Supply Chain" },
+              ]}
+            />
+          </Form.Item>
+
+          <Form.Item label="Project Name" name="deptName">
+            <Select
+              defaultValue="IT"
+              options={[
+                { value: "IT", label: "EX-212" },
+                { value: "Admin", label: "Pr-421" },
+                { value: "Supply Chain", label: "PR13244" },
+              ]}
+            />
+          </Form.Item>
+
+          
+
+          <Upload {...uploadProps}>
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
+          <Row>
+            <Col md={12} span={24} className="hp-pr-sm-0 hp-pr-12">
+              <Button
+                block
+                type="primary"
+                htmlType="submit"
+                onClick={updateUserInfo}
+              >
+                Submit
+              </Button>
+            </Col>
+
+            <Col md={12} span={24} className="hp-mt-sm-12 hp-pl-sm-0 hp-pl-12">
+              <Button block onClick={contactModalCancel}>
+                Cancel
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Modal>
       <div style={{ textAlign: "right", marginBottom: "16px" }}>
-        <Button type="primary">Add Documents</Button>
+        <Button type="primary" onClick={contactModalShow}>
+          Add Documents
+        </Button>
       </div>
       <Table columns={columns} dataSource={data} />
     </>
